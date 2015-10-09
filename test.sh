3d0f3b61ad842ced9eb047999c4f0b72ac3b3842
Sal <Sj.Randazzo@gmail.com>
2015-10-09 11:53:40 -0400

    Change test script

--- test.sh
+++ test.sh
@@ -2,4 +2,20 @@ workspace='Verona.xcworkspace'
 scheme='Verona'
 configuration='Debug'
 
-xcodebuild -workspace ${workspace} -scheme ${scheme} -configuration ${configuration} -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=9.0,name=iPhone 6' test
+set -o pipefail && xctool \
+-workspace ${workspace} \
+-scheme ${scheme} \
+-configuration ${configuration} \
+-sdk iphonesimulator \
+-destination 'platform=iOS Simulator,OS=9.0,name=iPhone 6s' \
+GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES \
+test
+
+#
+# xcodebuild  test \
+#   -workspace ${workspace} \
+#   -scheme ${scheme} \
+#   -configuration ${configuration}\
+#   -sdk iphonesimulator \
+#   -destination 'platform=iOS Simulator,OS=9.0,name=iPhone 6s' \
+#   -enableCodeCoverage YES
