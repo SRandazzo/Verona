006ca73d0afb8b3ceffbff3b96bf75590a7246b6
Sal <Sj.Randazzo@gmail.com>
2015-10-10 20:16:31 -0400

    Update travis.yml to show output of finding Coverage.profdata Use XCodeBuild instead of XCTool to run tests Update gitignore

--- test.sh
+++ test.sh
@@ -2,19 +2,18 @@ workspace='Verona.xcworkspace'
 scheme='Verona'
 configuration='Debug'
 
-set -o pipefail && xctool \
--workspace ${workspace} \
--scheme ${scheme} \
--configuration ${configuration} \
--sdk iphonesimulator \
--destination 'platform=iOS Simulator,OS=9.0,name=iPhone 6s' \
-test
+# xctool \
+# -workspace ${workspace} \
+# -scheme ${scheme} \
+# -configuration ${configuration} \
+# -sdk iphonesimulator \
+# -destination 'platform=iOS Simulator,OS=9.0,name=iPhone 6s' \
+# build test
 
-#
-# xcodebuild  test \
-#   -workspace ${workspace} \
-#   -scheme ${scheme} \
-#   -configuration ${configuration}\
-#   -sdk iphonesimulator \
-#   -destination 'platform=iOS Simulator,OS=9.0,name=iPhone 6s' \
-#   -enableCodeCoverage YES
+xcodebuild test \
+  -workspace ${workspace} \
+  -scheme ${scheme} \
+  -configuration ${configuration}\
+  -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,OS=9.0,name=iPhone 6s' \
+  -enableCodeCoverage YES
